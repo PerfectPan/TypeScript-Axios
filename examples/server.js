956ea786d51075548cfbda8931ab6be1a43d80cb
PerfectPan <3207113562@qq.com>
2019-11-30 22:26:36 +0800

    feat(examples): add new example for post method

--- examples/server.js
+++ examples/server.js
@@ -15,8 +15,25 @@ router.get('/simple/get', (req, res) => {
   });
 });
 
-router.get('/base/get', function(req, res) {
+router.get('/base/get', (req, res) => {
   res.json(req.query);
+});
+
+router.post('/base/post', (req, res) => {
+  res.json(req.body);
+});
+
+router.post('/base/buffer', (req, res) => {
+  const msg = [];
+  req.on('data', chunk => {
+    if (chunk) {
+      msg.push(chunk);
+    }
+  });
+  req.on('end', () => {
+    const buf = Buffer.concat(msg);
+    res.json(buf.toJSON());
+  });
 });
 
 app.use(router);
